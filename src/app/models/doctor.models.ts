026e578cb9fd7e12ec4295af48983df5ee70f605
Saikrishna <kopparapusaikrishna14@gmail.com>
2023-03-28 17:42:44 +0530

    added admin service-work in progress

---
+++ src/app/models/doctor.models.ts
@@ -0,0 +1,11 @@
+export interface Doctor {
+    name: string;
+    dob: Date;
+    gender: string;
+    email_id: string;
+    password: string;
+    qualification: string;
+    department_name: string;
+    phone_number: string;
+    clinic_address: string;
+}
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Doctors_lst } from '../models/doctors-lst.model';
+
+
+const baseUrl = 'http://localhost:8101';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  getDoctorsLst(): Observable<Doctors_lst> {
+    return this.http.get<Doctors_lst>(`${baseUrl}/GetDoctorsLst`);
+  }
+
+}
